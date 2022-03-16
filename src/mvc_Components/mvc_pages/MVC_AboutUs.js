db104bd39edf3c8df0e7b89b10702790d65d0b80
Farhad Tavakoli <72437846+Farhadtavakoli@users.noreply.github.com>
2022-03-16 13:22:53 +0100

    Test a link

--- src/mvc_Components/mvc_pages/MVC_AboutUs.js
+++ src/mvc_Components/mvc_pages/MVC_AboutUs.js
@@ -1,7 +1,17 @@
 import React from "react";
+import NavPane from "../Nav_pane";
+import { Link } from "react-router-dom";
 
 function MVC_AboutUs() {
-  return <h1>About Us</h1>;
+  return (
+    <>
+      <NavPane />
+      <h1>About us</h1>
+      <li className="menu-items">
+        <Link to="/">Home</Link>
+      </li>
+    </>
+  );
 }
 
 export default MVC_AboutUs;
