7b7c8b90345109e34e4bc7d5f7cbbb3b17045107
Farhad <farhad.tavakoli81@yahoo.com>
2022-05-03 14:12:53 +0200

    Background player is fixed

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ import MVC_Projects from "./mvc_Components/mvc_pages/MVC_Projects";
 import MVC_AboutUs from "./mvc_Components/mvc_pages/MVC_AboutUs";
 import MVC_Contacts from "./mvc_Components/mvc_pages/MVC_Contacts";
 import baseTheme from "./mvc_Components/mvc_pages/BaseTheme/Styles";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { deepmerge } from "@mui/utils";
 import { theme1, theme2 } from "./mvc_Components/mvc_pages/theme/AccessibilityThemes"
 import video from "./images/video-2.mp4";
-import VideoPlayer from "./mvc_Components/mvc_pages/videoBackground/VideoPlayer";
+//import VideoPlayer from "./mvc_Components/mvc_pages/videoBackground/VideoPlayer";
 
 import {
   Button,
@@ -26,21 +26,56 @@ import {
   ThemeProvider,
   createTheme
 } from "@mui/material";
-
-function App() {
-  /* Video*/
-
-  const videoElement = useRef();
-  const {
-    playerState,
-    togglePlay,
-    handleOnTimeUpdate,
-    handleVideoProgress,
-    handleVideoSpeed,
-    toggleMute,
-  } = VideoPlayer(videoElement);
+import { Player } from "video-react";
 
 
+
+function App(props) {
+
+  const [checked, setChecked] = React.useState(true);
+
+
+  const getVideo = useRef(null);
+  const [playerState, setPlayerState] = useState(
+    true
+  );
+  /* Video
+  const togglePlay = () => {
+    this.refs.vidRef.play();
+    setPlayerState(
+      !playerState
+ 
+    );
+  };
+
+  useEffect(() => {
+    playerState
+      ? this.refs.vidRef.play()
+      : this.refs.vidRef.pause();
+  }, [playerState.isPlaying, vidRef]);
+
+*/
+  const [playerMode_text, setPlayerMode_Text] = useState("AV")
+  const [playerMode, setPlayerMode] = useState(true);
+  console.log("Playermode is ::::::::> " + playerMode)
+  const playVideo = () => {
+    console.log("()=<")
+    getVideo.current.play()
+    setPlayerMode_Text("AV");
+  };
+
+  const pauseVideo = () => {
+    getVideo.current.pause();
+    setPlayerMode_Text("PÅ");
+  };
+
+  const togglePlay = () => {
+    setPlayerMode(!playerMode)
+    setChecked(!checked)
+    console.log("Playermode when toggling =======> " + playerMode)
+    playerMode ? pauseVideo() : playVideo()
+
+  };
 
 
   const [theme, setTheme] = useState(baseTheme);
@@ -53,7 +88,7 @@ function App() {
   // const [backgrond, setBackground] = useState("../images/video-1.mp4");
 
   /*function Change_BackgroundState() {
-
+ 
     if (backgrond === "../images/video-1.mp4") {
       setBackground("")
       localStorage.setItem("Background", backgrond)
@@ -63,9 +98,9 @@ function App() {
       localStorage.setItem("Background", backgrond)
     }
   }
-
-
-
+ 
+ 
+ 
   console.log("Before saving in storage " + backgrond);
   localStorage.setItem("Background", backgrond)*/
 
@@ -73,12 +108,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <div className="background_Container_Video">
-          <video controls loop
-            src={video}
-            ref={videoElement}
-            onTimeUpdate={handleOnTimeUpdate}
-          />
+        <div className="background_Container_Video" >
+          <video ref={getVideo} loop muted autoPlay>
+            <source src={video} />
+          </video>
+
         </div>
         {/** Navbar is placed here because it shoud be always on top of all elements in the page */}
         <NavPane />
@@ -86,7 +120,18 @@ function App() {
           <Button onClick={() => handleSwitch(JSON.parse(theme1))} variant='contained' className='accessibility_Setting_Btn'>Dark</Button>
           <Button onClick={() => handleSwitch(theme2)} variant='contained' className='accessibility_Setting_Btn'>Blind</Button>
           <Button onClick={() => handleSwitch((baseTheme))} variant='contained' className='accessibility_Setting_Btn'>Blind</Button>
-          <Button onClick={() => togglePlay()} variant='contained' className='accessibility_Setting_Btn'>Pausa video</Button>
+          {/*<Button onClick={() => { this.togglePlay.bind(this) }} variant='contained' className='accessibility_Setting_Btn'>Pausa video</Button>*/}
+          <Button onClick={() => togglePlay()} variant='contained' className='accessibility_Setting_Btn'>
+            {playerMode_text}
+          </Button>
+          <label>
+            <input type="checkbox"
+              defaultChecked={checked}
+              onChange={() => togglePlay()}
+            />
+            Check Me!
+          </label>
+
         </div>
         <Routes>
           <Route path="/" element={<MVC_Home />} />
@@ -100,3 +145,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
